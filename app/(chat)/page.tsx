b01fdf951b9c95bbf902b901d50509b8089018b4
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-05-09 20:19:37 +0200

    allow loggedin users only and add a switch for signup

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -4,6 +4,7 @@ import { AI } from '@/lib/chat/actions'
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
+import {redirect} from 'next/navigation';
 
 export const metadata = {
   title: 'Next.js AI Chatbot'
@@ -13,6 +14,15 @@ export default async function IndexPage() {
   const id = nanoid()
   const session = (await auth()) as Session
   const missingKeys = await getMissingKeys()
+
+  console.log('### session: ', session)
+
+  if (!session) {
+    console.log('### redirect to login: ', session)
+    redirect('/login')
+  }
+
+
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,6 +6,16 @@ import { redirect } from 'next/navigation'
 export default async function SignupPage() {
   const session = (await auth()) as Session
 
+  if (process.env.SIGNUP_ALLOWED !== 'true') {
+    return (
+      <main className="flex flex-col p-4">
+        <div className="w-full flex-1 px-6 pb-4 pt-8">
+          <h1 className="mb-3 text-2xl font-bold">Signup not allowed!</h1>
+        </div>  
+      </main>
+    )
+  }
+
   if (session) {
     redirect('/')
   }
