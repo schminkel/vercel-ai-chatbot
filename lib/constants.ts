5043b813f72c87d9d2a51129c129c688c2e6ee96
Thorsten Schminkel <thorsten@schminkel.de>
2025-07-20 10:00:38 +0200

    chore: comment out environment variable dumping code for clarity and maintainability

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,13 +1,13 @@
 import { generateDummyPassword } from './db/utils';
 
-// Dump all environment variables to the console on server start
-if (!(globalThis as any).__envDumped) {
-  (globalThis as any).__envDumped = true;
-  const filteredEnv = Object.fromEntries(
-    Object.entries(process.env).filter(([key]) => !key.startsWith('npm_package_'))
-  );
-  console.log('Environment Variables:', filteredEnv);
-}
+// // Dump all environment variables to the console on server start
+// if (!(globalThis as any).__envDumped) {
+//   (globalThis as any).__envDumped = true;
+//   const filteredEnv = Object.fromEntries(
+//     Object.entries(process.env).filter(([key]) => !key.startsWith('npm_package_'))
+//   );
+//   console.log('Environment Variables:', filteredEnv);
+// }
 
 export const isHTTPSUsageEnabled = process.env.NEXT_PUBLIC_HTTPS_USAGE === 'true'
 export const isProductionEnvironment = process.env.NODE_ENV === 'production';
